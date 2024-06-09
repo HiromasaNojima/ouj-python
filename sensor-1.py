bceb0c46e3b829a7af069e6db7b5ec51ece1eb6e
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2024-06-09 14:43:18 +0900

    first program

---
+++ sensor-1.py
@@ -0,0 +1,20 @@
+import Adafruit_DHT
+import time
+import csv
+
+sensor = Adafruit_DHT.DHT11
+pin = 4
+
+def get_sensor_data():
+    humidity, temperature = Adafruit_DHT.read(sensor, pin)
+    return humidity, temperature
+
+humidity, temperature = get_sensor_data()
+if humidity is not None and temperature is not None:
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+    print(f'Time: {timestamp}, Temperature: {temperature:.2f}C, Humidity: {humidity:.2f}%')
+    temperature_int = int(temperature)
+    humidity_int = int(humidity)
+    print(f'Time: {timestamp}, Temperature: {temperature_int}C, Humidity: {humidity_int}%')
+else:
+    print('Failed to get reading. Try again!')
