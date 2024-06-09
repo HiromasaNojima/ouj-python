02518133fafc55117b18518b210fa89f27efe8d3
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2024-06-09 15:16:37 +0900

    fix

--- sensor-1.py
+++ sensor-1.py
@@ -2,7 +2,7 @@ import Adafruit_DHT
 import time
 
 sensor = Adafruit_DHT.DHT22
-pin = 3
+pin = 2
 
 def get_sensor_data():
     humidity, temperature = Adafruit_DHT.read(sensor, pin)
@@ -11,7 +11,7 @@ def get_sensor_data():
 humidity, temperature = get_sensor_data()
 if humidity is not None and temperature is not None:
     timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-    print(f'Time: {timestamp}, Temperature: {temperature:.2f}C, Humidity: {humidity:.2f}%')
+    print(f'Time: {timestamp}, Temperature: {temperature:.1f}C, Humidity: {humidity:.1f}%')
     temperature_int = int(temperature)
     humidity_int = int(humidity)
     print(f'Time: {timestamp}, Temperature: {temperature_int}C, Humidity: {humidity_int}%')
